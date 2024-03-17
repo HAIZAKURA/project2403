5746cad0cd2b1f2725f3f99f480746179c3ce6dc
HAIZAKURA <hhbilly99@gmail.com>
2024-03-17 21:00:52 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,2 @@
-// let json = {
-//     // "msg_id":0,
-//     "msg_code": 0,
-//     "measure_ground_res_time": 0,
-//     "mqtt_upload_time": 0,
-//     "ground_res_correction": 0,
-// }
-
-// if (json.hasOwnProperty("msg_id")) {
-//     console.log("1")
-// } else {
-//     console.log("2")
-// }
-
-import { md5 } from 'js-md5';
-
-let a = "111"
-let b = "222"
-let c = a + b
-console.log(c)
-console.log(md5(c))
-console.log(md5(a+b));
-
+let str = Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+console.log(str);
