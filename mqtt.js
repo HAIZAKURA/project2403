78c9b5c36e7c1533cb4c6270750c176b08ece0aa
HAIZAKURA <hhbilly99@gmail.com>
2024-03-17 20:48:24 +0800

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -6,13 +6,15 @@ import { logger } from "./app.js";
 
 // MQTT连接建立
 const mqtt_host = process.env.MQTT_HOST || "mqtt://sh.nya.run";
+const mqtt_port = process.env.MQTT_PORT || 38883;
+const connectUrl = `${mqtt_host}:${mqtt_port}`
 const clientId = "mysql_adapter_" + Math.random().toString(16).substring(2, 8);
-const client = mqtt.connect(mqtt_host, {
+const client = mqtt.connect(connectUrl, {
     clientId
 });
 
 // MQTT订阅主题
-const topic = ["device_report", "cloud/#"];
+const topic = ["device_report", "/a13jYFS3MfN/+/user/update"];
 const qos = 2
 client.subscribe(topic, { qos }, (err) => {
     if (err) {
@@ -26,7 +28,7 @@ async function insert_into_box_log(data) {
     
 }
 
-// 异步将数据插入到泄漏日志中
+// 异步将数据插入到漏电保护设备日志中
 async function insert_into_leakage_log(data) {
     try {
         // 使用LeakageLog模型创建一个新的日志条目并插入到数据库中
