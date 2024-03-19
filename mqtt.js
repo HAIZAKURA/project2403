aa4476b6f6494b3f9a83985a8f9fd03df2221ca7
HAIZAKURA <hhbilly99@gmail.com>
2024-03-19 19:11:00 +0800

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -1,6 +1,7 @@
 import mqtt from "mqtt";
-import { LeakageLog } from './model.js';
+import { Box, BoxLog, BoxState, LeakageLog } from './model.js';
 import { logger } from "./app.js";
+import crc16 from 'node-crc16';
 
 /** MQTT2MySQL Begin **/
 
@@ -11,6 +12,11 @@ const connectUrl = `${mqtt_host}:${mqtt_port}`
 const clientId = "mysql_adapter_" + Math.random().toString(16).substring(2, 8);
 const client = mqtt.connect(connectUrl, {
     clientId
+});
+
+// MQTT连接建立成功
+client.on("connect", () => {
+    logger.info("MQTT connected");
 });
 
 // MQTT订阅主题
@@ -24,12 +30,12 @@ client.subscribe(topic, { qos }, (err) => {
     logger.info("Subscribed to topic " + topic);
 })
 
-async function insert_into_box_log(data) {
-    
-}
+/** ============================================== **/
+/** ================以下漏电保护部分================= **/
+/** ============================================== **/
 
 // 异步将数据插入到漏电保护设备日志中
-async function insert_into_leakage_log(data) {
+async function leakage_insert_log(data) {
     try {
         // 使用LeakageLog模型创建一个新的日志条目并插入到数据库中
         await LeakageLog.create({
@@ -46,19 +52,407 @@ async function insert_into_leakage_log(data) {
     }
 }
 
-async function insert_into_box_alert(data) {
+async function leakage_insert_alert(data) {
     
 }
 
-async function insert_into_leakage_alert(data) {
+/**
+ * 设置漏电保护设备参数
+ * @param {string} id 漏电保护设备ID
+ * @param {number} measure_ground_res_time 测量接地电阻的时间
+ * @param {number} mqtt_upload_time MQTT消息上传时间
+ * @param {number} ground_res_correction 接地电阻修正值
+ * @returns {boolean} 成功发布消息返回true，否则返回false
+ */
+async function set_leakage(id, measure_ground_res_time, mqtt_upload_time, ground_res_correction) {
+    // 检查ID是否为非空字符串
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light On: Invalid ID provided.", id);
+        return false;
+    }
+    let data = {
+        "leakage": {
+            "msg_id": 0,
+            "msg_code": 0,
+        }
+    };
+    // 根据条件添加测量接地电阻时间和MQTT上传时间到data对象
+    if (typeof measure_ground_res_time == 'number' || measure_ground_res_time >= 0) {
+        data.leakage.measure_ground_res_time = measure_ground_res_time;
+    }
+    if (typeof mqtt_upload_time == 'number' || mqtt_upload_time >= 0) {
+        data.leakage.mqtt_upload_time = mqtt_upload_time;
+    }
+    if (typeof ground_res_correction == 'number') {
+        data.leakage.ground_res_correction = ground_res_correction;
+    }
+    try {
+        let topic = "cloud/" + id;
+        // 尝试将数据发布到指定的MQTT主题
+        await client.publishAsync(topic, data, { qos: 2 });
+        return true; // 消息发布成功
+    } catch(err) {
+        logger.error(err); // 记录发布的异常错误
+        return false; // 发布失败
+    }
+}
+
+/**
+ * 测量漏电保护设备接地电阻
+ * @param {string} id 漏电保护设备ID
+ * @returns {boolean} 如果消息发布成功返回true，否则返回false。
+ */
+async function measure_res(id) {
+    // 检查ID是否为非空字符串
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light On: Invalid ID provided.", id);
+        return false;
+    }
+    try {
+        let topic = "cloud/" + id;
+        let data = {
+            "leakage": {
+                "get_ground_res": 1
+            }
+        };
+        // 尝试将数据发布到指定的MQTT主题
+        await client.publishAsync(topic, data, { qos: 2 });
+        return true; // 消息发布成功
+    } catch(err) {
+        logger.error(err); // 记录发布的异常错误
+        return false; // 发布失败
+    }
+}
+
+/** ============================================== **/
+/** ==================以下云盒部分================== **/
+/** ============================================== **/
+
+
+// 辅助函数：将数字转换为两位的十六进制字符串
+function padToTwoDigits(value) {
+    return value.toString(16).padStart(2, '0').toUpperCase();
+}
+
+// 辅助函数：将数字转换为四位的十六进制字符串
+function padToFourDigits(value) {
+    return value.toString(16).padStart(4, '0').toUpperCase();
+}
+
+async function light_insert_log(data) {
     
+}
+
+async function light_insert_alert(data) {
+    
+}
+
+/**
+ * 打开指定ID的灯
+ * @param {string} id 设备ID
+ * @param {number} brightness 亮度，0-100
+ * @returns {boolean} - 如果消息成功发布，则返回true；否则返回false。
+ */
+async function light_on(id, brightness) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light On: Invalid ID provided.", id);
+        return false;
+    }
+    // 检查亮度是否有效
+    if (typeof brightness !== 'number' || brightness < 0 || brightness > 100) {
+        brightness = 100;
+    }
+    try {
+        // 构造MQTT消息的主题
+        let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+        // 构造亮度字符串
+        let brightnessStr = padToTwoDigits(brightness);
+        // 构造控制灯的命令字符串
+        let cmdStr = "AA00" + id.toUpperCase() + "A104010101" + brightnessStr;
+        // 计算命令字符串的校验和
+        let sum = crc16.checkSum(cmdStr);
+        // 将命令字符串和校验和转换为Buffer格式，以供发送
+        let cmd = Buffer.from(cmdStr + sum, "hex");
+        // 尝试发布消息到MQTT主题
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true; // 消息发布成功
+    } catch (err) {
+        logger.error(err); // 捕获异常并记录错误
+        return false; // 异常发生，消息发布失败
+    }
+}
+
+/**
+ * 关闭指定ID的灯
+ * @param {string} id 设备ID
+ * @returns {boolean} 如果消息成功发布，则返回true；否则返回false。
+ */
+async function light_off(id) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light Off: Invalid ID provided.", id);
+        return false;
+    }
+    // 构造MQTT消息的主题
+    let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+    // 构造控制灯的命令字符串
+    let cmdStr = "AA00" + id.toUpperCase() + "A10401010000";
+    // 计算命令字符串的校验和
+    let sum = crc16.checkSum(cmdStr);
+    // 将命令字符串和校验和转换为Buffer格式，以供发送
+    let cmd = Buffer.from(cmdStr + sum, "hex");
+    try {
+        // 尝试发布消息到MQTT主题
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true; // 消息发布成功
+    } catch (err) {
+        logger.error(err); // 捕获异常并记录错误
+        return false; // 异常发生，消息发布失败
+    }
+}
+
+/**
+ * 设置时间策略
+ * @param {string} id 设备ID
+ * @param {object} data 包含设置时间所需的数据对象
+ * @returns {boolean} 成功发布消息到MQTT主题返回true，否则返回false
+ */
+async function light_set_time(id, data) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light Set Time: Invalid ID provided.", id);
+        return false;
+    }
+    // 检查时间设置数据是否有效
+    if (typeof data !== 'object' || data === null) {
+        logger.error("Light Set Time: Invalid data provided.", data);
+        return false;
+    }
+    // 构建MQTT消息的主题
+    let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+    // 将小时、分钟和设置的数据转换为16进制字符串，并确保它们是两位数
+    let hour = padToTwoDigits(data.hour);
+    let minute = padToTwoDigits(data.minute);
+    let s1_t = padToFourDigits(data.s1.t);
+    let s1_b = padToTwoDigits(data.s1.b);
+    let s2_t = padToFourDigits(data.s2.t);
+    let s2_b = padToTwoDigits(data.s2.b);
+    let s3_t = padToFourDigits(data.s3.t);
+    let s3_b = padToTwoDigits(data.s3.b);
+    let s4_t = padToFourDigits(data.s4.t);
+    let s4_b = padToTwoDigits(data.s4.b);
+    // 构造控制灯的命令字符串
+    let cmdStr = "AA00" + id.toUpperCase() + + "A2100101" + hour + minute + s1_t + s1_b + s2_t + s2_b + s3_t + s3_b + s4_t + s4_b;
+    // 计算命令字符串的校验和
+    let sum = crc16.checkSum(cmdStr);
+    // 将命令字符串和校验和转换为Buffer，以便发送
+    let cmd = Buffer.from(cmdStr + sum, "hex");
+    try {
+        // 尝试通过MQTT发布消息
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true; // 消息发布成功
+    } catch (err) {
+        logger.error(err); // 记录异常和错误
+        return false; // 消息发布失败
+    }
+}
+
+/**
+ * 查询时间策略
+ * @param {string} id 设备ID
+ * @returns {boolean} 如果消息发布成功返回true，否则返回false
+ */
+async function light_query_time(id) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light Query Time: Invalid ID provided.", id);
+        return false;
+    }
+    // 构造MQTT消息的主题
+    let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+    // 构造控制灯的命令字符串
+    let cmdStr = "AA00" + id.toUpperCase() + "A300";
+    // 计算命令字符串的校验和
+    let sum = crc16.checkSum(cmdStr);
+    // 将命令字符串和校验和转换为Buffer，以便发送
+    let cmd = Buffer.from(cmdStr + sum, "hex");
+    try {
+        // 尝试通过MQTT发布消息
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true; // 消息发布成功
+    } catch (err) {
+        logger.error(err); // 记录异常和错误
+        return false; // 消息发布失败
+    }
+}
+
+/**
+ * 更新时间策略
+ * @param {string} id 设备ID
+ * @param {Array} data 包含灯光设置数据的数组，每个元素都是一个16进制字符串
+ * @returns {Promise<void>} 不返回任何内容
+ */
+async function light_update_time(id, data) {
+    // 将传入的16进制字符串数组转换为对应的整数，用于设置时间策略参数
+    let hour = parseInt(data[0], 16);
+    let minute = parseInt(data[1], 16);
+    let s1_t = parseInt(data[2], 16);
+    let s1_b = parseInt(data[3], 16);
+    let s2_t = parseInt(data[4], 16);
+    let s2_b = parseInt(data[5], 16);
+    let s3_t = parseInt(data[6], 16);
+    let s3_b = parseInt(data[7], 16);
+    let s4_t = parseInt(data[8], 16);
+    let s4_b = parseInt(data[9], 16);
+    
+    try {
+        // 使用await关键字暂停执行，直到数据库更新完成。尝试更新指定时间策略
+        await Box.update({
+                t_hour: hour,
+                t_minute: minute,
+                s1_t: s1_t,
+                s1_b: s1_b,
+                s2_t: s2_t,
+                s2_b: s2_b,
+                s3_t: s3_t,
+                s3_b: s3_b,
+                s4_t: s4_t,
+                s4_b: s4_b
+            }, {
+                where: {
+                    box_id: id
+                }
+        });
+    } catch (err) {
+        // 如果在更新过程中出现错误，将错误记录到日志中
+        logger.error(err);
+    }
+}
+
+/**
+ * 查询 电压 电流 电量值
+ * @param {string} id 设备ID
+ * @returns {boolean} 如果消息发布成功则返回true，否则返回false
+ */
+async function light_query_power(id) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light Query Time: Invalid ID provided.", id);
+        return false;
+    }
+
+    // 构造MQTT消息的主题
+    let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+
+    // 构造控制灯的命令字符串
+    let cmdStr = "AA00" + id.toUpperCase() + "A400";
+
+    // 计算命令字符串的校验和
+    let sum = crc16.checkSum(cmdStr);
+
+    // 将命令字符串和校验和转换为Buffer，以便发送
+    let cmd = Buffer.from(cmdStr + sum, "hex");
+
+    try {
+        // 尝试通过MQTT发布消息
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true; // 消息发布成功
+    } catch (err) {
+        logger.error(err); // 记录异常和错误
+        return false; // 消息发布失败
+    }
+}
+
+/**
+ * 记录 电压 电流 电量值
+ * @param {string} id 设备ID
+ * @param {Array} data 包含电压、电流和电量值的数组，每个元素以十六进制字符串形式表示
+ * @returns {Promise<void>} 不返回任何内容
+ */
+async function light_insert_power(id, data) {
+    // 将数据从十六进制字符串转换为数值
+    let vol = parseInt(data[0], 16);
+    let cur = parseInt(data[1], 16);
+    let pow = parseInt(data[2], 16) * 0.01;
+    // 获取当前时间的UTC时间戳
+    let time_utc = Math.floor(Date.now() / 1000);
+    try {
+        // 尝试在数据库中创建并插入一个新的日志条目
+        await BoxLog.create({
+            box_id: id,
+            VOL: vol,
+            CUR: cur,
+            POW: pow,
+            time_utc: time_utc
+        });
+    } catch (err) {
+        // 如果遇到错误，将错误信息记录到日志中
+        logger.error(err);
+    }
+}
+
+/**
+ * 更新设备状态
+ * @param {string} id 设备ID
+ * @param {Array} data 包含状态和亮度的数组，其中第一个元素是状态值，第二个元素是亮度值，都以16进制字符串形式提供
+ * @returns {void} 不返回任何内容
+ */
+async function light_update_state(id, data) {
+    // 将数据数组的第一个元素解析为状态整数，第二个元素解析为亮度整数
+    let state = parseInt(data[0], 16);
+    let brightness = parseInt(data[1], 16);
+    try {
+        // 尝试通过BoxState模型，更新或插入灯光的状态和亮度信息
+        await BoxState.upsert({
+            box_id: id,
+            state: state,
+            brightness: brightness
+        });
+    } catch (err) {
+        // 如果遇到错误，将错误记录到日志中
+        logger.error(err);
+    }
+}
+
+/**
+ * 查询设备状态
+ * @param {string} id 设备ID
+ * @returns {boolean} 如果消息成功发布，则返回true；否则返回false
+ */
+async function light_query_state(id) {
+    // 检查ID是否有效
+    if (typeof id !== 'string' || id.trim() === '') {
+        logger.error("Light Query Time: Invalid ID provided.", id);
+        return false;
+    }
+    // 构造MQTT消息的主题
+    let topic = "/a13jYFS3MfN/" + id.toUpperCase() + "/user/get";
+    // 构造控制灯的命令字符串
+    let cmdStr = "AA00" + id.toUpperCase() + "A900";
+    // 计算命令字符串的校验和
+    let sum = crc16.checkSum(cmdStr);
+    // 将命令字符串和校验和转换为Buffer，以便发送
+    let cmd = Buffer.from(cmdStr + sum, "hex");
+    try {
+        // 尝试通过MQTT发布消息
+        await client.publishAsync(topic, cmd, { qos: 2 });
+        return true;
+    } catch (err) {
+        logger.error(err);
+        return false;
+    }
 }
 
 // MQTT消息处理
 client.on("message", (topic, message) => {
     if (topic == "device_report") {
         let payload = JSON.parse(message.toString()).leakage;
-        insert_into_leakage_log(payload);
+        leakage_insert_log(payload);
+    } else if (topic.startsWith("/a13jYFS3MfN/")) {
+        let id = topic.split("/")[2];
+        let msg = message.toString("hex").toUpperCase();
+        console.log(msg);
     }
 });
 
