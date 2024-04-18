bb1fd3a48ce077d7f350194e6ba79f35eb01fdbe
HAIZAKURA <hhbilly99@gmail.com>
2024-04-18 14:12:38 +0900

    Update box.js

--- router/box.js
+++ router/box.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import { logger } from '../app.js';
 import { Box } from '../model.js';
+import { authenticateToken } from "../tool/auth.js";
 
 const router = express.Router();
 
@@ -23,41 +24,33 @@ const router = express.Router();
  * @param {object} req - 请求对象，包含查询参数和会话信息。
  * @param {object} res - 响应对象，用于返回查询结果或错误信息。
  */
-router.get('', async (req, res) => {
+router.get('', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            let where_clause = {}; // 用于构建查询条件的对象
+        let where_clause = {}; // 用于构建查询条件的对象
 
-            // 根据查询参数添加到where_clause对象中
-            if (req.query.region_id) {
-                where_clause.region_id = req.query.region_id;
-            }
-            if (req.query.road_id) {
-                where_clause.road_id = req.query.road_id;
-            }
+        // 根据查询参数添加到where_clause对象中
+        if (req.query.region_id) {
+            where_clause.region_id = req.query.region_id;
+        }
+        if (req.query.road_id) {
+            where_clause.road_id = req.query.road_id;
+        }
 
-            // 查询满足条件的所有Box信息
-            let box = await Box.findAll({
-                where: where_clause,
-                attributes: ['box_id', 'leakage_id', 'light_id', 'latitude', 'longitude', 'region_id', 'road_id']
-            });
+        // 查询满足条件的所有Box信息
+        let box = await Box.findAll({
+            where: where_clause,
+            attributes: ['box_id', 'leakage_id', 'light_id', 'latitude', 'longitude', 'region_id', 'road_id']
+        });
 
-            // 根据查询结果返回相应信息
-            if (box) {
-                res.json({
-                    code: 200,
-                    data: box
-                });
-            } else {
-                res.json({
-                    code: 400
-                });
-            }
-        } else {
-            // 如果用户未登录，返回相应错误信息
+        // 根据查询结果返回相应信息
+        if (box) {
             res.json({
-                code: 401
+                code: 200,
+                data: box
+            });
+        } else {
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
@@ -75,23 +68,15 @@ router.get('', async (req, res) => {
  * @param {Object} req - 请求对象，包含 route 参数和 session 信息。
  * @param {Object} res - 响应对象，用于返回查询结果或错误信息。
  */
-router.get('/:box_id', async (req, res) => {
+router.get('/:box_id', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 通过 box_id 查询 Box 表，获取对应 box 信息
-            let box = await Box.findByPk(req.params.box_id);
-            // 返回查询结果
-            res.json({
-                code: 200,
-                data: box
-            });
-        } else {
-            // 用户未登录，返回 401 错误码
-            res.json({
-                code: 401
-            });
-        }
+        // 通过 box_id 查询 Box 表，获取对应 box 信息
+        let box = await Box.findByPk(req.params.box_id);
+        // 返回查询结果
+        res.json({
+            code: 200,
+            data: box
+        });
     } catch (error) {
         // 捕获并记录错误信息
         logger.error('Error:', error);
@@ -109,34 +94,26 @@ router.get('/:box_id', async (req, res) => {
  * @param {Object} req 请求对象，包含用户信息及POST请求的数据。
  * @param {Object} res 响应对象，用于返回处理结果。
  */
-router.post('', async (req, res) => {
+router.post('', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 创建新的Box实例
-            let box = await Box.create({
-                box_id: req.body.box_id,
-                leakage_id: req.body.leakage_id,
-                light_id: req.body.light_id,
-                latitude: req.body.latitude,
-                longitude: req.body.longitude,
-                region_id: req.body.region_id,
-                road_id: req.body.road_id
-            });
-            // 根据创建结果返回相应的状态码
-            if (box) {
-                res.json({
-                    code: 200
-                });
-            } else {
-                res.json({
-                    code: 400
-                });
-            }
-        } else {
-            // 如果用户未登录，返回状态码401
+        // 创建新的Box实例
+        let box = await Box.create({
+            box_id: req.body.box_id,
+            leakage_id: req.body.leakage_id,
+            light_id: req.body.light_id,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude,
+            region_id: req.body.region_id,
+            road_id: req.body.road_id
+        });
+        // 根据创建结果返回相应的状态码
+        if (box) {
             res.json({
-                code: 401
+                code: 200
+            });
+        } else {
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
@@ -154,36 +131,28 @@ router.post('', async (req, res) => {
  * @param {Object} req - 请求对象，包含盒子的更新信息和认证会话
  * @param {Object} res - 响应对象，用于返回操作结果
  */
-router.put('/:box_id', async (req, res) => {
+router.put('/:box_id', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 尝试更新盒子信息
-            let box = await Box.update({
-                light_id: req.body.light_id, // 灯光ID
-                latitude: req.body.latitude, // 纬度
-                longitude: req.body.longitude, // 经度
-                region_id: req.body.region_id, // 区域ID
-                road_id: req.body.road_id // 路段ID
-            }, {
-                where: {
-                    box_id: req.params.box_id // 盒子ID
-                }
-            });
-            // 成功更新则返回200，否则返回400
-            if (box) {
-                res.json({
-                    code: 200
-                });
-            } else {
-                res.json({
-                    code: 400
-                });
+        // 尝试更新盒子信息
+        let box = await Box.update({
+            light_id: req.body.light_id, // 灯光ID
+            latitude: req.body.latitude, // 纬度
+            longitude: req.body.longitude, // 经度
+            region_id: req.body.region_id, // 区域ID
+            road_id: req.body.road_id // 路段ID
+        }, {
+            where: {
+                box_id: req.params.box_id // 盒子ID
             }
-        } else {
-            // 未登录返回401
+        });
+        // 成功更新则返回200，否则返回400
+        if (box) {
             res.json({
-                code: 401
+                code: 200
+            });
+        } else {
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
@@ -202,31 +171,23 @@ router.put('/:box_id', async (req, res) => {
  * @param {Object} res - 响应对象，用于返回操作结果
  * @returns {Object} 返回一个包含操作状态码的JSON对象
  */
-router.delete('/:box_id', async (req, res) => {
+router.delete('/:box_id', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 根据提供的box_id删除盒子信息
-            let box = await Box.destroy({
-                where: {
-                    box_id: req.params.box_id
-                }
-            });
-            // 如果删除成功，返回成功状态码
-            if (box) {
-                res.json({
-                    code: 200
-                });
-            } else {
-                // 如果删除失败（例如不存在该盒子），返回失败状态码
-                res.json({
-                    code: 400
-                });
+        // 根据提供的box_id删除盒子信息
+        let box = await Box.destroy({
+            where: {
+                box_id: req.params.box_id
             }
-        } else {
-            // 如果用户未登录，返回未授权状态码
+        });
+        // 如果删除成功，返回成功状态码
+        if (box) {
             res.json({
-                code: 401
+                code: 200
+            });
+        } else {
+            // 如果删除失败（例如不存在该盒子），返回失败状态码
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
