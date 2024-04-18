b843213f4674d6448860185989e757146d067364
HAIZAKURA <hhbilly99@gmail.com>
2024-04-18 14:12:33 +0900

    Update box_state.js

--- router/box_state.js
+++ router/box_state.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import { logger } from '../app.js';
 import { BoxState } from '../model.js';
+import { authenticateToken } from "../tool/auth.js";
 
 const router = express.Router();
 
@@ -24,31 +25,23 @@ const router = express.Router();
  * @param {object} res 响应对象，用于返回处理结果
  * @returns {object} 返回JSON格式的响应数据，包含状态码和数据
  */
-router.get('', async (req, res) => {
+router.get('', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 查询BoxState信息，按box_id升序排列
-            let box_state = await BoxState.findAll({
-                order: [
-                    ['box_id', 'ASC']
-                ]
-            });
-            // 判断查询结果，返回相应状态码和数据
-            if (box_state) {
-                res.json({
-                    code: 200,
-                    data: box_state
-                });
-            } else {
-                res.json({
-                    code: 400
-                });
-            }
-        } else {
-            // 用户未登录，返回未授权状态码
+        // 查询BoxState信息，按box_id升序排列
+        let box_state = await BoxState.findAll({
+            order: [
+                ['box_id', 'ASC']
+            ]
+        });
+        // 判断查询结果，返回相应状态码和数据
+        if (box_state) {
             res.json({
-                code: 401
+                code: 200,
+                data: box_state
+            });
+        } else {
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
@@ -67,28 +60,20 @@ router.get('', async (req, res) => {
  * @param {Object} res 响应对象，用于返回状态码和数据
  * @returns {Object} 返回包含状态码和数据的json对象。成功时，状态码为200，数据为box状态；未登录时，状态码为401；找不到box状态时，状态码为400；操作异常时，状态码为400。
  */
-router.get('/:box_id', async (req, res) => {
+router.get('/:box_id', authenticateToken, async (req, res) => {
     try {
-        // 检查用户是否已登录
-        if (req.session.isLogin) {
-            // 通过 box_id 查找盒子状态
-            let box_state = await BoxState.findByPk(req.params.box_id);
-            if (box_state) {
-                // 找到盒子状态，返回状态码和数据
-                res.json({
-                    code: 200,
-                    data: box_state
-                });
-            } else {
-                // 未找到盒子状态，返回状态码400
-                res.json({
-                    code: 400
-                });
-            }
-        } else {
-            // 用户未登录，返回状态码401
+        // 通过 box_id 查找盒子状态
+        let box_state = await BoxState.findByPk(req.params.box_id);
+        if (box_state) {
+            // 找到盒子状态，返回状态码和数据
             res.json({
-                code: 401
+                code: 200,
+                data: box_state
+            });
+        } else {
+            // 未找到盒子状态，返回状态码400
+            res.json({
+                code: 400
             });
         }
     } catch (error) {
